cc5a8c2370869a6eb6b6a8456e4737e28b40127c
jaderyan <jadeelizabethryan@outlook.com>
2020-04-23 11:13:03 +0100

    not found styling

--- src/views/NotFound/NotFound.tsx
+++ src/views/NotFound/NotFound.tsx
@@ -27,7 +27,7 @@ const NotFound: FunctionComponent<RouteComponentProps> = ({ history }) => (
           src={Page404Large}
           className="mobile-show not-found--icon--mobile"
         />
-        <h1 className="home--welcome--title">
+        <h1 className="not-found--title">
           {cms("not-found.title")}
           <ReactSVG
             src={Page404}
@@ -36,13 +36,13 @@ const NotFound: FunctionComponent<RouteComponentProps> = ({ history }) => (
           />
         </h1>
 
-        <p className="home--welcome--about">{cms("not-found.content")}</p>
+        <p className="not-found--content">{cms("not-found.content")}</p>
       </div>
     </div>
     <div className="flex-container flex-container--no-padding flex-container--full-width  home--experiences--container">
       <div className="flex-col--12 home--experiences">
         <div className="flex-container flex-container--center flex-container--justify">
-          <div className="flex-col--7 flex-col--tablet-large--12">
+          <div className="flex-col--6 flex-col--tablet-large--12">
             <h3 className="home--experiences--title">
               {cms("home.experiences.title")}
             </h3>
@@ -51,29 +51,40 @@ const NotFound: FunctionComponent<RouteComponentProps> = ({ history }) => (
             </p>
             <Button
               text="Browse experiences"
-              onClick={() => console.log("browse experiences")}
+              onClick={() =>
+                history.push({
+                  pathname: "/browse",
+                })
+              }
             />
           </div>
         </div>
       </div>
     </div>
     <div className="flex-container flex-container--center flex-container--align-center home--share">
-      <div className="flex-col--mobile--12 flex-col--tablet--12 flex-col--6">
+      <div className="flex-col--12">
         <h4 className="home--share--title">{cms("home.share.title")}</h4>
+      </div>
+      <div className="flex-col--mobile--12 flex-col--tablet--12 flex-col--6">
+        <div className="flex-col--tablet--10 flex-col--mobile--12 flex-col--mobile-small--12 home--share--description--container">
+          <div className="home--share--description">
+            <ReactSVG src={Pencil} wrapper="span" />
+            <h5 style={{ display: "inline" }}>
+              {cms("home.share.experiences")}
+            </h5>
+          </div>
 
-        <div className="home--share--description">
-          <ReactSVG src={Pencil} />
-          <p> {cms("home.share.experiences")}</p>
-        </div>
+          <div className="home--share--description">
+            <ReactSVG src={Community} wrapper="span" />
+            <h5 style={{ display: "inline" }}>{cms("home.share.community")}</h5>
+          </div>
 
-        <div className="home--share--description">
-          <ReactSVG src={Community} />
-          <p>{cms("home.share.community")}</p>
-        </div>
-
-        <div className="home--share--description">
-          <ReactSVG src={Security} />
-          <p>{cms("home.share.contributions")}</p>
+          <div className="home--share--description">
+            <ReactSVG src={Security} wrapper="span" />
+            <h5 style={{ display: "inline" }}>
+              {cms("home.share.contributions")}
+            </h5>
+          </div>
         </div>
       </div>
       <div className="flex-col--mobile--12 flex-col--tablet--12 flex-col--5 home--share--button-container">
@@ -81,10 +92,10 @@ const NotFound: FunctionComponent<RouteComponentProps> = ({ history }) => (
           text="Sign up and share"
           onClick={() =>
             history.push({
-              pathname: "/register"
+              pathname: "/register",
             })
           }
-          twoCol={true}
+          navy={true}
         />
         <p className="home--share--content">{cms("home.share.content")}</p>
       </div>
@@ -93,7 +104,10 @@ const NotFound: FunctionComponent<RouteComponentProps> = ({ history }) => (
       <div className="flex-container flex-container--center home--footer">
         <div className="flex-col--8 flex-col--tablet-large--12">
           <h5 className="home--footer--title">{cms("home.footer.title")}</h5>
-          <p className="home--footer--content">{cms("home.footer.content")}</p>
+          <p
+            className="home--footer--content"
+            dangerouslySetInnerHTML={{ __html: cms("home.footer.content") }}
+          />
         </div>
         <div className="flex-col--8 flex-col--tablet-large--12">
           <p className="home--footer--contact">
