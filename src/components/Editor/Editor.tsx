b0b5f5bbfa910a0c0c42445679fdbe5b975500f3
Jade <jadeelizabethryan@outlook.com>
2020-04-23 12:17:51 +0100

    allow editor to show correct font weights (#10)

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -13,29 +13,30 @@ const toolbarConfig = {
   display: [
     "INLINE_STYLE_BUTTONS",
     "BLOCK_TYPE_BUTTONS",
-    "BLOCK_TYPE_DROPDOWN"
+    "BLOCK_TYPE_DROPDOWN",
   ],
   INLINE_STYLE_BUTTONS: [
     { label: "Bold", style: "BOLD", className: "editor--button" },
-    { label: "Italic", style: "ITALIC", className: "editor--button" }
+    { label: "Italic", style: "ITALIC", className: "editor--button" },
   ],
   BLOCK_TYPE_DROPDOWN: [
     { label: "Normal", style: "unstyled" },
-    { label: "Heading", style: "header-one" }
+    { label: "Heading", style: "header-one" },
   ],
   BLOCK_TYPE_BUTTONS: [
-    { label: "UL", style: "unordered-list-item", className: "editor--button" }
-  ]
+    { label: "UL", style: "unordered-list-item", className: "editor--button" },
+  ],
 };
 
 const Editor: FunctionComponent<IProps> = ({ text, onChange }) => (
   <RichTextEditor
     value={text}
-    onChange={text => onChange(text)}
+    onChange={(text) => onChange(text)}
     // @ts-ignore
     toolbarConfig={toolbarConfig}
     toolbarClassName="editor--toolbar"
     editorClassName="editor--content"
+    onPaste
   />
 );
 
