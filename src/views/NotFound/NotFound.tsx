b1ccca508fbc8e9f9f9d606b9f5cd6d9ae1fa658
jaderyan <jadeelizabethryan@outlook.com>
2020-10-03 14:04:20 +0100

    tidy up imports

--- src/views/NotFound/NotFound.tsx
+++ src/views/NotFound/NotFound.tsx
@@ -7,11 +7,7 @@ import { cms } from "../../utils/cms";
 
 import "./NotFound.scss";
 
-import Footer from "../../components/Footer";
 import Button from "../../components/Button";
-import Pencil from "../../assets/icons/pencil.svg";
-import Community from "../../assets/icons/community.svg";
-import Security from "../../assets/icons/security.svg";
 import Page404Large from "../../assets/icons/page-404-large.svg";
 import Page404 from "../../assets/icons/page-404.svg";
 import Layout from "../../components/Layout";
@@ -61,62 +57,6 @@ const NotFound: FunctionComponent<RouteComponentProps> = ({ history }) => (
         </div>
       </div>
     </div>
-    {/* <div className="flex-container flex-container--center flex-container--align-center home--share">
-      <div className="flex-col--12">
-        <h4 className="home--share--title">{cms("home.share.title")}</h4>
-      </div>
-      <div className="flex-col--mobile--12 flex-col--tablet--12 flex-col--6">
-        <div className="flex-col--tablet--10 flex-col--mobile--12 flex-col--mobile-small--12 home--share--description--container">
-          <div className="home--share--description">
-            <ReactSVG src={Pencil} wrapper="span" />
-            <h5 style={{ display: "inline" }}>
-              {cms("home.share.experiences")}
-            </h5>
-          </div>
-
-          <div className="home--share--description">
-            <ReactSVG src={Community} wrapper="span" />
-            <h5 style={{ display: "inline" }}>{cms("home.share.community")}</h5>
-          </div>
-
-          <div className="home--share--description">
-            <ReactSVG src={Security} wrapper="span" />
-            <h5 style={{ display: "inline" }}>
-              {cms("home.share.contributions")}
-            </h5>
-          </div>
-        </div>
-      </div>
-      <div className="flex-col--mobile--12 flex-col--tablet--12 flex-col--5 home--share--button-container">
-        <Button
-          text="Sign up and share"
-          onClick={() =>
-            history.push({
-              pathname: "/register",
-            })
-          }
-          navy={true}
-        />
-        <p className="home--share--content">{cms("home.share.content")}</p>
-      </div>
-    </div>
-    <Footer purple={true}>
-      <div className="flex-container flex-container--center home--footer">
-        <div className="flex-col--8 flex-col--tablet-large--12">
-          <h5 className="home--footer--title">{cms("home.footer.title")}</h5>
-          <p
-            className="home--footer--content"
-            dangerouslySetInnerHTML={{ __html: cms("home.footer.content") }}
-          />
-        </div>
-        <div className="flex-col--8 flex-col--tablet-large--12">
-          <p className="home--footer--contact">
-            {cms("home.footer.contact")}{" "}
-            <a href={`mailto:${cms("global.email")}`}>{cms("global.email")}</a>
-          </p>
-        </div>
-      </div>
-    </Footer> */}
   </Layout>
 );
 
